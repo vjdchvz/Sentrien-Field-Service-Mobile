b22708aa55ba63348b292d78ef48f42770cfe5d9
Kurisu081299 <robertchristian.rosales@gmail.com>
2023-01-05 01:52:34 +0800

    contents

--- sfs/app/src/main/java/com/example/sfs/MainActivity.kt
+++ sfs/app/src/main/java/com/example/sfs/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.sfs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
     }
 }
