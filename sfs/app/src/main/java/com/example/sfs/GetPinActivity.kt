7093c3e43bc05c97746843351ff35aa2049b0f79
vjdchvz <victorjhondechavez@gmail.com>
2023-01-06 00:47:54 +0800

    otp UI Complete

--- sfs/app/src/main/java/com/example/sfs/GetPinActivity.kt
+++ sfs/app/src/main/java/com/example/sfs/GetPinActivity.kt
@@ -7,5 +7,7 @@ class GetPinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_pin)
+
+        
     }
 }
