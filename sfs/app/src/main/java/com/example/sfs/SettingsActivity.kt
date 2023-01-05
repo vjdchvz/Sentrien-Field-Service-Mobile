3cbd62c236f3bb09ab0d50539906ec5af7f6a613
Kurisu081299 <robertchristian.rosales@gmail.com>
2023-01-06 04:53:12 +0800

    id inputs

--- sfs/app/src/main/java/com/example/sfs/SettingsActivity.kt
+++ sfs/app/src/main/java/com/example/sfs/SettingsActivity.kt
@@ -26,15 +26,60 @@ class SettingsActivity : AppCompatActivity() {
     }
     private fun showEditTextDialog(){
         val empID_diag = findViewById<View>(R.id.empID) as Button
-        val test1 = findViewById<View>(R.id.test)
         empID_diag.setOnClickListener {
             val builder = AlertDialog.Builder(this)
             val inflater = LayoutInflater.from(this)
             val dialogLayout = inflater.inflate(R.layout.edit_text_layout, null)
-            val editText = dialogLayout.findViewById<EditText>(R.id.empID_input)
+            val editText = dialogLayout.findViewById<EditText>(R.id.input)
             val test1 = findViewById<TextView>(R.id.test)
             with(builder){
                 setTitle("Enter Employee ID")
+                setPositiveButton("OK"){dialog, which ->
+                    test1.text = editText.text.toString()
+
+                }
+                setNegativeButton("Cancel"){dialog, which ->
+                    Log.d("Main","Negative button clicked")
+
+                }
+                setView(dialogLayout)
+                show()
+            }
+
+        }
+
+        val truckID_diag = findViewById<View>(R.id.truckID) as Button
+        truckID_diag.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val inflater = LayoutInflater.from(this)
+            val dialogLayout = inflater.inflate(R.layout.edit_text_layout, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.input)
+            val test1 = findViewById<TextView>(R.id.test)
+            with(builder){
+                setTitle("Enter Truck ID")
+                setPositiveButton("OK"){dialog, which ->
+                    test1.text = editText.text.toString()
+
+                }
+                setNegativeButton("Cancel"){dialog, which ->
+                    Log.d("Main","Negative button clicked")
+
+                }
+                setView(dialogLayout)
+                show()
+            }
+
+        }
+
+        val key = findViewById<View>(R.id.licensekey) as Button
+        key.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            val inflater = LayoutInflater.from(this)
+            val dialogLayout = inflater.inflate(R.layout.edit_text_layout, null)
+            val editText = dialogLayout.findViewById<EditText>(R.id.input)
+            val test1 = findViewById<TextView>(R.id.test)
+            with(builder){
+                setTitle("Enter License Key")
                 setPositiveButton("OK"){dialog, which ->
                     test1.text = editText.text.toString()
 
