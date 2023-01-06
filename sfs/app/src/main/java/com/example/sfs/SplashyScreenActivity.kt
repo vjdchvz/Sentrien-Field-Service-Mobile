01972cc275843f371e85b0df0635bb02ddeb9a41
vjdchvz <victorjhondechavez@gmail.com>
2023-01-07 04:22:47 +0800

    otpminorupdates

--- sfs/app/src/main/java/com/example/sfs/SplashyScreenActivity.kt
+++ sfs/app/src/main/java/com/example/sfs/SplashyScreenActivity.kt
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import kotlinx.coroutines.*
 
 class SplashyScreenActivity : AppCompatActivity() {
+    @OptIn(DelicateCoroutinesApi::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashyscreen)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent1 = Intent(this, GetPinActivity::class.java)
-            startActivity(intent1)
-            overridePendingTransition(android.R.anim.bounce_interpolator,android.R.anim.bounce_interpolator)
+            GlobalScope.launch(Dispatchers.IO) {
+                // heavy initialization tasks or network requests here
 
-            // to get rid of this when u push the back button
-            finish()
+                withContext(Dispatchers.Main) {
+                    val intent1 = Intent(this@SplashyScreenActivity, GetPinActivity::class.java)
+                    startActivity(intent1)
+                    overridePendingTransition(
+                        android.R.anim.bounce_interpolator,
+                        android.R.anim.bounce_interpolator
+                    )
+                    finish()
+                }
+            }
         }, 2000)
 
     }
-    }
+}
